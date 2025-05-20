c7636c79bfef213cb14cd42de80d4f8f6d41dfee
Amidope <66752876+Amidope@users.noreply.github.com>
2025-05-20 17:44:20 +0500

    add fillable and relations

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'content',
+        'post_id',
+        'user_id'
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }

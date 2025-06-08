4cf5e7004cb99b7c4669922663bc8750dcfb83e6
Amidope <66752876+Amidope@users.noreply.github.com>
2025-06-08 10:55:08 +0500

    fill post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
+use App\Services\PostService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PostController extends Controller
 {
@@ -12,15 +16,16 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return Post::latest('created_at')->paginate(10);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request, PostService $service)
     {
-        //
+        $post = $service->create($request->validated());
+        return response()->json($post, status: 201);
     }
 
     /**
@@ -28,15 +33,16 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return $post;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Post $post)
+    public function update(UpdatePostRequest $request, Post $post, PostService $service)
     {
-        //
+        $service->update($request->validated(), $post);
+        return response()->json($post);
     }
 
     /**
@@ -44,6 +50,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response(status: 204);
     }
 }

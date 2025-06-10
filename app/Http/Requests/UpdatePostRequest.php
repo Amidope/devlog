d32e8415396fcbee0df2da7e6d6803d9d28e7578
Amidope <66752876+Amidope@users.noreply.github.com>
2025-06-10 13:33:31 +0500

    fix unique rule

--- app/Http/Requests/UpdatePostRequest.php
+++ app/Http/Requests/UpdatePostRequest.php
@@ -3,17 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdatePostRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -22,7 +15,11 @@ class UpdatePostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|max:255|unique:posts',
+            'title' => [
+                'required',
+                'max:255',
+                Rule::unique('posts', 'title')->ignore($this->route('post')),
+            ],
             'body' => 'required'
         ];
     }

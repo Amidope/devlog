24711ca73c9187c0587dc8f5f61a051f02bf4309
Amidope <66752876+Amidope@users.noreply.github.com>
2025-06-10 13:34:05 +0500

    fill comment controller

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -10,33 +13,35 @@ class CommentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Post $post)
     {
-        //
+        return CommentResource::collection(
+            $post->comments()->with('user')
+            ->latest('created_at')
+            ->paginate(10)
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CommentRequest $request, Post $post)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Comment $comment)
-    {
-        //
+        $comment = Comment::create([
+            ...$request->validated(),
+            'post_id' => $post->id,
+            'user_id' => $request->user()->id
+        ]);
+        return response($comment, 201);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Comment $comment)
+    public function update(CommentRequest $request, Comment $comment)
     {
-        //
+        $comment->update($request->validated());
+        return response()->json($comment);
     }
 
     /**
@@ -44,6 +49,7 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
-        //
+        $comment->delete();
+        return response(status: 204);
     }
 }

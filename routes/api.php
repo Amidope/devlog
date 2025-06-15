1109e12b88eab36b4ba665725d3a4f9c8e71889b
Amidope <66752876+Amidope@users.noreply.github.com>
2025-06-15 13:07:41 +0500

    add auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
@@ -9,6 +10,11 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// add admin middleware
 Route::apiResources([
     'posts' => PostController::class,
 ]);

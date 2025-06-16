26894512d5a8b76c9024eaea902d77e51d5f2302
Amidope <66752876+Amidope@users.noreply.github.com>
2025-06-16 13:03:35 +0500

    revert breeze install changes

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);

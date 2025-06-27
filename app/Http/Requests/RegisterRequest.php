0f10265981b01b39ffd9c9239fa2c8ad13f6c045
Amidope <66752876+Amidope@users.noreply.github.com>
2025-06-27 14:03:13 +0500

    rm symbols requirement

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,6 @@ class RegisterRequest extends FormRequest
                     ->letters()
                     ->mixedCase()
                     ->numbers()
-                    ->symbols()
             ],
         ];
     }

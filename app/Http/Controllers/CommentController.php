a2177bc65a5440c476d11598d568ccae8684f820
Amidope <66752876+Amidope@users.noreply.github.com>
2025-07-05 12:40:03 +0500

    authorize comment resource with policy

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -10,6 +10,12 @@ use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->authorizeResource(Comment::class);
+    }
+
     /**
      * Display a listing of the resource.
      */

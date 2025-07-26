41338c5fe2bbb4b73c5ddee47f2465c77d6e9d06
Amidope <66752876+Amidope@users.noreply.github.com>
2025-07-26 18:46:40 +0500

    add paginated index with cache

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -3,9 +3,18 @@
 namespace App\Services;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 
 class PostService
 {
+    public function getPostsPage(int $page)
+    {
+        return Cache::tags('posts')->remember(
+            "posts_page_{$page}",
+            600,
+            fn() => Post::latest('created_at')->paginate(10)
+        );
+    }
 
     public function create(array $data): Post
     {

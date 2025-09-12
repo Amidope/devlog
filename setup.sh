854931008889bf4b2cda366e2e5d9678a5de8032
Amidope <66752876+Amidope@users.noreply.github.com>
2025-09-12 19:17:30 +0500

    append UID/GID from the current shell if they’re missing

--- setup.sh
+++ setup.sh
@@ -3,3 +3,11 @@
 if [ ! -f .env ]; then
   cp .env.example .env
 fi
+
+if ! grep -q '^UID=' .env; then
+  echo "UID=$(id -u)" >> .env
+fi
+
+if ! grep -q '^GID=' .env; then
+  echo "GID=$(id -g)" >> .env
+fi
